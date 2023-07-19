b2b3303e314ffbef5bcd8ef312d611928d2dc2fd
Vineet Bogam <114532660+V15247@users.noreply.github.com>
2023-07-18 21:05:37 -0400

    Implemented ability for users to register (backend)

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose')
 const app = express();
-
+const authRoutes = require('./routes/auth.js');
 
 //Connect to MongoDB
 dotenv.config();
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true } );
+
+//Middlewares
+app.use('/api/users', authRoutes);
 
 
 app.listen(3000, () => {
---
+++ server/routes/auth.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();  
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+
+
+router.post('/register', jsonParser, async (req,res) => {
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    try{
+        const savedUser = await user.save();
+        res.send({user: user._id});
+    }catch(err) {
+        res.status(400).send(err);
+    }
+});
+
+module.exports = router;
