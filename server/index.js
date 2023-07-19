89352819c7d39f9a4036bad4ef31cdb89bcabaf3
Vineet Bogam <114532660+V15247@users.noreply.github.com>
2023-07-18 20:54:08 -0400

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 const app = express();
+
+
+//Connect to MongoDB
+dotenv.config();
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true } );
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
